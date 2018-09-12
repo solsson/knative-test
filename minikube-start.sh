ed7c365cfb966756460e288c48e63572b0809ac7
Staffan Olsson <staffan@repos.se>
2018-09-12 10:54:45 +0200

    Latest k8s 1.10.x

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,5 +1,5 @@
 minikube start --memory=10240 --cpus=4 \
-  --kubernetes-version=v1.10.6 \
+  --kubernetes-version=v1.10.7 \
   --vm-driver=hyperkit \
   --bootstrapper=kubeadm \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
