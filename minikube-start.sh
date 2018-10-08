cfd00238ea5367438582858d208a8d2015c2dff4
Staffan Olsson <staffan@repos.se>
2018-10-08 09:09:35 +0200

    Try 1.11.x

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,5 +1,5 @@
 minikube start --memory=10240 --cpus=4 \
-  --kubernetes-version=v1.10.8 \
+  --kubernetes-version=v1.11.3 \
   --vm-driver=hyperkit \
   --bootstrapper=kubeadm \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
