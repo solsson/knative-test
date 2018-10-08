77f139f83832d24c4980ebc29ced781cec587f34
Staffan Olsson <staffan@repos.se>
2018-10-08 09:09:23 +0200

    Removes controller-manager options as,
    according to https://istio.io/docs/setup/kubernetes/platform-setup/minikube/,
    we don't need them for 1.10+

--- minikube-start.sh
+++ minikube-start.sh
@@ -3,5 +3,4 @@ minikube start --memory=10240 --cpus=4 \
   --vm-driver=hyperkit \
   --bootstrapper=kubeadm \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
-  --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key" \
-  --extra-config=apiserver.admission-control="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+  --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key"
