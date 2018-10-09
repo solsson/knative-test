90517f0b5a647ba727adcb76741890d736331eb2
Staffan Olsson <staffan@repos.se>
2018-10-09 10:38:45 +0200

    Use k8s 1.11.x because eventing commits talk about the upgrade

--- minikube-start.sh
+++ minikube-start.sh
@@ -3,7 +3,7 @@ minikube version | grep v0.29 && echo "Note: We've had issues with controller-ma
 minikube version | grep v0.30 && echo "Note: We've had issues with controller-manager crashlooping on Minikube 0.30"
 
 minikube start --memory=8192 --cpus=4 \
-  --kubernetes-version=v1.10.8 \
+  --kubernetes-version=v1.11.3 \
   --vm-driver=hyperkit \
   --bootstrapper=kubeadm \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
