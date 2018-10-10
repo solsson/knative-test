248d81dcdc543f91aa336d82daa412dd006ec35b
Staffan Olsson <staffan@repos.se>
2018-10-10 07:42:43 +0200

    Back to more memory to make sure it doesn't restrict eventing

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,7 +1,7 @@
 
 minikube version | grep v0.28 && echo "You might need extra args for <0.29 minikube, see https://github.com/istio/istio.io/pull/2708"
 
-minikube start --memory=8192 --cpus=4 \
+minikube start --memory=10240 --cpus=4 \
   --kubernetes-version=v1.11.3 \
   --vm-driver=hyperkit \
   --bootstrapper=kubeadm
