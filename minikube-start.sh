35a0aa76b8a0188d48fb94dfe7baef00342dae9c
Staffan Olsson <staffan@repos.se>
2018-10-10 07:42:09 +0200

    Solves the controller-manager crashloop

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,10 +1,7 @@
 
-minikube version | grep v0.29 && echo "Note: We've had issues with controller-manager crashlooping on Minikube 0.29"
-minikube version | grep v0.30 && echo "Note: We've had issues with controller-manager crashlooping on Minikube 0.30"
+minikube version | grep v0.28 && echo "You might need extra args for <0.29 minikube, see https://github.com/istio/istio.io/pull/2708"
 
 minikube start --memory=8192 --cpus=4 \
   --kubernetes-version=v1.11.3 \
   --vm-driver=hyperkit \
-  --bootstrapper=kubeadm \
-  --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
-  --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key"
+  --bootstrapper=kubeadm
